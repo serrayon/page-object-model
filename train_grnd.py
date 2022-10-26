48b04c1797f6779ad5f2341462890d04d87a77e7
Serrayon <serrayon81@gmail.com>
2022-10-26 15:37:05 -0700

    page object model for trial of stones updated

---
+++ train_grnd.py
@@ -0,0 +1,17 @@
+from selenium .webdriver.common.by import By
+from base_element import BaseElement
+from base_page import BasePage
+
+
+class TrainingGroundPage(BasePage):
+    
+    url = 'https://techstepacademy.com/training-ground'
+        
+    @property
+    def button1(self):
+        locator =(By.ID, 'bi'),
+        return BaseElement(
+            driver = self.driver,
+            by=locator[0],
+            value = locator[1]
+        )  
